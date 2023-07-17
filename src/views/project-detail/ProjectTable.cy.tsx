6dd73245f4580bd21574d00d486b5d9d8a77efcd
주은 <00-10-21@naver.com>
2023-07-17 16:34:34 +0900

    test: Delete unnecessary test code

--- src/views/project-detail/ProjectTable.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ProjectTable from './ProjectTable'
-
-describe('<ProjectTable />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<ProjectTable />)
-  })
-})
