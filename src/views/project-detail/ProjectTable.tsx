012d8d03ba5ede5d09e196bfefa1387fc4856bac
주은 <00-10-21@naver.com>
2023-07-17 16:35:10 +0900

    feat: Delete Status and Insert Author in project table

--- src/views/project-detail/ProjectTable.tsx
+++ src/views/project-detail/ProjectTable.tsx
@@ -13,8 +13,8 @@ import Typography from '@mui/material/Typography'
 import CategoryTag, { Category } from 'src/views/project-detail/CategoryTag'
 import StatusTag, { Status } from 'src/views/project-detail/StatusTag'
 
-const createData = (releaseNoteId: number, version: string, releaseTitle: string, changeItems: ChangeItem[], status: Status, creationDate: string) => {
-  return { releaseNoteId, version, releaseTitle, changeItems, status, creationDate }
+const createData = (releaseNoteId: number, version: string, releaseTitle: string, changeItems: ChangeItem[], creatorId: string, creationDate: string) => {
+  return { releaseNoteId, version, releaseTitle, changeItems, creatorId, creationDate }
 }
 
 
@@ -25,10 +25,10 @@ interface ChangeItem {
 
 
 const rows = [
-  createData(4, 'v 1.2.1', 'BB 기능 추가 및 변경', [{ category: Category.Feature, releaseContent: 'bb 기능 - 기능 설명' }, { category: Category.Changed, releaseContent: '타입 변경' }], Status.InProgress, '2023.06.30'),
-  createData(3, 'v 1.1.2', 'OO 서비스 중단', [{ category: Category.Deprecated, releaseContent: '~API 중단' }], Status.Completed, '2023.06.29'),
-  createData(2, 'v 1.1.1', 'AA 기능 수정 및 버그 해결', [{ category: Category.Fixed, releaseContent: '~ 연동 안되는 버그 수정' }, { category: Category.Changed, releaseContent: '~ 성능 개선' }], Status.Completed, '2023.06.26'),
-  createData(1, 'v 1.0.0', '문서 최초 생성', [{ category: Category.Feature, releaseContent: '신규 기능 추가 / AA 기능 제공' }], Status.Completed, '2023.06.20')
+  createData(4, 'v 1.2.1', 'BB 기능 추가 및 변경', [{ category: Category.Feature, releaseContent: 'bb 기능 - 기능 설명' }, { category: Category.Changed, releaseContent: '타입 변경' }], '유소연', '2023.06.30'),
+  createData(3, 'v 1.1.2', 'OO 서비스 중단', [{ category: Category.Deprecated, releaseContent: '~API 중단' }], '이소현', '2023.06.29'),
+  createData(2, 'v 1.1.1', 'AA 기능 수정 및 버그 해결', [{ category: Category.Fixed, releaseContent: '~ 연동 안되는 버그 수정' }, { category: Category.Changed, releaseContent: '~ 성능 개선' }], '이서빈', '2023.06.26'),
+  createData(1, 'v 1.0.0', '문서 최초 생성', [{ category: Category.Feature, releaseContent: '신규 기능 추가 / AA 기능 제공' }], '장예경', '2023.06.20')
 ]
 
 
@@ -42,7 +42,7 @@ const ProjectTable = () => {
             <TableCell>번호</TableCell>
             <TableCell align='left'>버전</TableCell>
             <TableCell align='left'>변경사항</TableCell>
-            <TableCell align='left'>작업상태</TableCell>
+            <TableCell align='left'>작성자</TableCell>
             <TableCell align='left'>날짜</TableCell>
           </TableRow>
         </TableHead>
@@ -70,7 +70,7 @@ const ProjectTable = () => {
                 ))}
               </TableCell>
               <TableCell align='left'>
-                <StatusTag status={row.status} />
+                {row.creatorId}
               </TableCell>
               <TableCell align='left'>{row.creationDate}</TableCell>
             </TableRow>
