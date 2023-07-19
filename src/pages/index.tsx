829dc91d42a921b41ec9bd7db254a77bbe9ff478
jykyeong99 <2009skylove@naver.com>
2023-07-19 11:48:32 +0900

    refactor: Modify starting page(homepage) & logo change
    1. Home에 사이드메뉴,appBar 안보이도록
       경로가 "/"일 때 {children}만 나오도록 수정 (UserLayout.tsx)
    2. logo svg 파일 추가하여 로고 변경 -> 코드 간소화

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,45 @@
+import Image from 'next/image'
+
 // ** MUI Imports
-import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import Button, { ButtonProps }  from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import { styled } from '@mui/material/styles'
 
-// ** Styled Component Import
-import ApexChartWrapper from 'src/@core/styles/libs/react-apexcharts'
+// ** Styled Components
+const ButtonStyled = styled(Button)<ButtonProps>(({ theme }) => ({
+  margin: theme.spacing(4.5),
+  width: 150,
+}))
 
-// **Components Imports
-import Project from 'src/views/dashboard/Project'
+const HomePage = () => {
 
-const Dashboard = () => {
   return (
-    <ApexChartWrapper>
-      <Grid container spacing={6}>
-        <Grid item xs={12}>
-          <Project/>
-        </Grid>
-      </Grid>
-    </ApexChartWrapper>
+    <Box className='content-center' sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Image src="/images/LogBook_Logo(1).svg" alt="Logo" width={400} height={200} />
+      <Typography variant='body2'>Work on your project efficiently with the release note sharing system!</Typography>
+      <Box>
+        <ButtonStyled variant='contained'>
+          Sign up
+          <input
+            hidden
+            type='file'
+            accept='image/png, image/jpeg'
+            id='account-settings-upload-image'
+          />
+        </ButtonStyled>
+        <ButtonStyled variant='contained'>
+          Login
+          <input
+            hidden
+            type='file'
+            accept='image/png, image/jpeg'
+            id='account-settings-upload-image'
+          />
+        </ButtonStyled>
+      </Box>
+    </Box>
   )
 }
 
-export default Dashboard
+export default HomePage
