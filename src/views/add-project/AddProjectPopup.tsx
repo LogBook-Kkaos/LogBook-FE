d083cbd9a69793e0a9cbc7eed5d1336305c1a966
jykyeong99 <2009skylove@naver.com>
2023-08-01 17:28:17 +0900

    feat: search, invite, delete User
    사용자 검색, auto complete로 수정(api도 전체 유저조회로 변경)
    유저 초대시 members에 추가
    삭제 시 다시 allusers에 추가
    검색 결과는 이름순으로 정렬

--- src/views/add-project/AddProjectPopup.tsx
+++ src/views/add-project/AddProjectPopup.tsx
@@ -1,39 +1,34 @@
-import React, { ReactNode, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
-import { styled, Theme } from '@mui/material/styles'
-import useMediaQuery from '@mui/material/useMediaQuery'
+import { styled } from '@mui/material/styles'
 import Grid from '@mui/material/Grid'
 import Dialog from '@mui/material/Dialog'
-import { DialogActions, DialogContent, DialogTitle, Divider } from '@mui/material';
+import { DialogActions, DialogContent, DialogTitle, Divider } from '@mui/material'
 import TextField from '@mui/material/TextField'
-import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
+import Autocomplete, { AutocompleteRenderOptionState, createFilterOptions } from '@mui/material/Autocomplete'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
+import ListItem from '@mui/material/ListItem'
 import InputAdornment from '@mui/material/InputAdornment'
-import MuiMenu, { MenuProps } from '@mui/material/Menu'
-import MuiMenuItem, { MenuItemProps } from '@mui/material/MenuItem'
 import Typography, { TypographyProps } from '@mui/material/Typography'
 
 import Magnify from 'mdi-material-ui/Magnify'
 
 // ** HTTP Client
-import axios from 'axios';
-
-// ** Third Party Components
-import PerfectScrollbarComponent from 'react-perfect-scrollbar'
+import axios from 'axios'
 
 interface AddProjectPopupProps {
    isOpen: boolean
    onClose: () => void
 }
 
-interface SearchResultItem {
-  userName: string | null;
-  email: string;
+interface UserInfo {
+  userName: string
+  email: string
 }
 
 interface SearchResults {
-  result: SearchResultItem[];
+  result: UserInfo[]
 }
 
 const StyledDialog = styled(Dialog)(({ theme }) => ({
@@ -45,42 +40,10 @@ const StyledDialog = styled(Dialog)(({ theme }) => ({
       maxHeight: "560px"
     }
   }
-}));
-
-const Menu = styled(MuiMenu)<MenuProps>(({ theme }) => ({
-  '& .MuiMenu-paper': {
-    width: 380,
-    overflow: 'hidden',
-    marginTop: theme.spacing(4),
-    [theme.breakpoints.down('sm')]: {
-      width: '100%'
-    }
-  },
-  '& .MuiMenu-list': {
-    padding: 0
-  }
 }))
-
-const MenuItem = styled(MuiMenuItem)<MenuItemProps>(({ theme }) => ({
-  paddingTop: theme.spacing(3),
-  paddingBottom: theme.spacing(3),
-  borderBottom: `1px solid ${theme.palette.divider}`,
-  display:'flex'
-}))
-
-const styles = {
-  maxHeight: 349,
-  '& .MuiMenuItem-root:last-of-type': {
-    border: 0
-  }
-}
-
-const PerfectScrollbar = styled(PerfectScrollbarComponent)({
-  ...styles
-})
 
 // ** Styled component for the title in MenuItems
-const MenuItemTitle = styled(Typography)<TypographyProps>(({ theme }) => ({
+const ListTitle = styled(Typography)<TypographyProps>(({ theme }) => ({
   fontWeight: 600,
   flex: '1 1 100%',
   overflow: 'hidden',
@@ -91,80 +54,94 @@ const MenuItemTitle = styled(Typography)<TypographyProps>(({ theme }) => ({
 }))
 
 // ** Styled component for the subtitle in MenuItems
-const MenuItemSubtitle = styled(Typography)<TypographyProps>({
+const ListSubtitle = styled(Typography)<TypographyProps>({
   flex: '1 1 100%',
   overflow: 'hidden',
   whiteSpace: 'nowrap',
   textOverflow: 'ellipsis'
 })
 
-const MenuItemWrapper = styled(Box)({
-  width: '100%',
-  display: 'flex',
-  alignItems: 'center'
-})
-
 const AddProjectPopup = (props: AddProjectPopupProps) => {
   const { isOpen, onClose } = props
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   
   const [projectName, setProjectName] = useState('')
   const [projectDescription, setProjectDescription] = useState('')
-  const [members, setMembers] = useState<string[]>([]);
   const [searchTerm, setSearchTerm] = useState('')
-  const [searchResults, setSearchResults] = useState<SearchResults>({ result: [] });
+  const [members, setMembers] = useState<UserInfo[]>([])
+  const [allUsers, setAllUsers] = useState<UserInfo[]>([])
 
   const handleProjectNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setProjectName(event.target.value)
-  };
+  }
 
   const handleProjectDescriptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setProjectDescription(event.target.value)
-  };
+  }
 
-  // ** Hook
-  const hidden = useMediaQuery((theme: Theme) => theme.breakpoints.down('lg'))
+  const handleInvitation = (event: React.MouseEvent, option: UserInfo) => {
+    event.stopPropagation()
+    setAllUsers((prevAllUsers) =>
+      prevAllUsers.filter((user) => user.userName !== option.userName)
+    )
+    setMembers((prevMembers) => [...prevMembers, option])
+  }
 
-  const ScrollWrapper = ({ children }: { children: ReactNode }) => {
-    if (hidden) {
-      return <Box sx={{ ...styles, overflowY: 'auto', overflowX: 'hidden' }}>{children}</Box>
-    } else {
-      return (
-        <PerfectScrollbar options={{ wheelPropagation: false, suppressScrollX: true }}>{children}</PerfectScrollbar>
-      )
+  const handleDeleteButtonClick = (event: React.MouseEvent) => {
+    const indexToDelete = event.currentTarget.getAttribute('data-index')
+    if (indexToDelete !== null) {
+      const memberToDelete = members[parseInt(indexToDelete)]
+      handleDeleteMember(memberToDelete)
     }
   }
 
-  useEffect(()=>{
-    const fetchSearchResults = async() =>{
-      try{
-        const response = await axios.get<SearchResults>(`/api/users/search?keyword=${searchTerm}`);
-        setSearchResults(response.data);
-        console.log(response.data);
-      } catch (error) {
-        console.error('Error fetching search results:', error);
-        setSearchResults({result:[]});
-      }
-    };
+  const handleDeleteMember = (member: UserInfo) => {
+    setMembers((prevMembers) => prevMembers.filter((m) => m.userName !== member.userName))
+    setAllUsers((prevAllUsers) => {
+      const updatedAllUsers = [...prevAllUsers, member];
+      return sortUsersByName(updatedAllUsers);
+    })
+  }
 
-    const searchDelay = setTimeout(()=>{
-      fetchSearchResults();
-    },500);
-
-    return () => clearTimeout(searchDelay);
-  }, [searchTerm]);
-
+  // 권한 변경 handler로 변경필요
   const handleSaveMember = () => {
-    // 저장 버튼을 클릭할 때 실행되는 함수
-    if (projectName) {
-      setMembers([...members, projectName]); // 새로운 멤버 정보를 배열에 추가
-    }
-  };
+    
+  }
   
   const handleClickAddButton = (event: React.MouseEvent<HTMLButtonElement>) => {
-    console.log('Adding project:', projectName);
+    // 프로젝트 생성 api
     onClose();
   }
+
+  const searchResults = (props: React.HTMLAttributes<HTMLLIElement>, option: UserInfo, state: AutocompleteRenderOptionState) => (
+    <ListItem {...props} style={{justifyContent:'space-between'}}>
+      <Box>
+        <ListTitle>{option.userName}</ListTitle>
+        <ListSubtitle>{option.email}</ListSubtitle>
+      </Box>
+      <Button variant="outlined" color="primary" onClick={(event) => handleInvitation(event, option)}>
+        초대
+      </Button>
+    </ListItem>
+  )
+
+  const sortUsersByName = (users: UserInfo[]) => {
+    return users.sort((a, b) => a.userName.localeCompare(b.userName))
+  }
+
+  useEffect(() => {
+    const fetchAllUsers = async () => {
+      try {
+        const response = await axios.get<SearchResults>('/api/users');
+        const sortingResult = sortUsersByName(response.data.result);
+        setAllUsers(sortingResult);
+      } catch (error) {
+        console.error('Error fetching all users:', error);
+        setAllUsers([]);
+      }
+    }
+  
+    fetchAllUsers();
+  }, [isOpen])
 
   return (
     <StyledDialog open={isOpen} onClose={onClose} >
@@ -176,93 +153,71 @@ const AddProjectPopup = (props: AddProjectPopupProps) => {
       <DialogContent sx={{pl:10, pr:7}}>
         <Grid container spacing={2} sx={{alignItems: 'center'}} >
           <Grid item xs={12}>
-          <TextField
-          label="프로젝트명"
-          value={projectName}
-          onChange={handleProjectNameChange}
-          variant='standard'
-          sx={{width:'400px', mb:3}}
-          />
+            <TextField
+            label="프로젝트명"
+            value={projectName}
+            onChange={handleProjectNameChange}
+            variant='standard'
+            sx={{width:'400px', mb:3}}
+            />
           </Grid>
           <Grid item xs={12}>
-          <TextField
-          label="설명"
-          value={projectDescription}
-          onChange={handleProjectDescriptionChange}
-          variant='standard'  
-          multiline
-          sx={{width:'400px', mb:10}}
-          />
+            <TextField
+            label="설명"
+            value={projectDescription}
+            onChange={handleProjectDescriptionChange}
+            variant='standard'  
+            multiline
+            sx={{width:'400px', mb:10}}
+            />
           </Grid>
+
           <Grid item xs={6} >
-          <Typography sx={{ fontSize: 20, fontWeight: 600 }}>멤버 추가</Typography>
+            <Typography sx={{ fontSize: 20, fontWeight: 600 }}>멤버 추가</Typography>
           </Grid>
           <Grid item xs={6}>
-            <TextField
-            size='small'
-            sx={{ '& .MuiOutlinedInput-root': { borderRadius: 4 }, ml: 9 }}
-            placeholder='Search'
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position='start'>
-                  <Magnify fontSize='small' />
-                </InputAdornment>
-              )
-            }}
-            onFocus={(e) => setAnchorEl(e.currentTarget)}
+            <Autocomplete
+              size="small"
+              options={allUsers}
+              getOptionLabel={(option) => option.email}
+              inputValue={searchTerm}
+              onInputChange={(event, newValue) => setSearchTerm(newValue)}
+              filterOptions={createFilterOptions({
+                stringify: (option: UserInfo) => option.email,
+              })}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  sx={{ '& .MuiOutlinedInput-root': { borderRadius: 4 }}}
+                  autoComplete="off"
+                  placeholder="Search"
+                  InputProps={{
+                    ...params.InputProps,
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <Magnify fontSize="small" />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              )}
+              renderOption={searchResults}
+              ListboxProps={{
+                style: {
+                  maxHeight: 200,
+                },
+              }}
             />
-            <Menu
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={() => setAnchorEl(null)}
-              anchorReference="anchorEl"
-              //anchorPosition={{ top: 800, left: 270 }}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-            >
-              <ScrollWrapper>
-              {searchResults.result.map((item, index) => (
-                <MenuItem key={index} onClick={() => setAnchorEl(null)}>
-                {/* <MenuItem key={index} onClick={() => handleResultItemClick(result)}> */}
-                  <MenuItemTitle>{item.email}</MenuItemTitle>
-                  <MenuItemSubtitle>{item.email}</MenuItemSubtitle>
-                </MenuItem>
-              ))}
-              </ScrollWrapper>
-            </Menu>
           </Grid>
+
           <Grid item xs={12}>
             <Divider/>
           </Grid>
-
-          <Grid item xs={8}>
-            <Typography>member1</Typography>
-          </Grid>
-          <Grid item xs={4}>
-            <Button
-              variant="outlined"
-              onClick={handleSaveMember}
-              color="primary"
-              sx={{ mr: 3, ml: 12 }}
-            >
-            저장
-            </Button>
-            <Button variant="outlined" color="error">
-              삭제
-            </Button>
-          </Grid>
+          
           {members.map((member, index) => (
-            <>
-            <Grid item xs={8}>
-            <Typography>{member}</Typography>
+          <>
+          <Grid item xs={8}>
+            <Typography>{member.userName}</Typography>
           </Grid>
           <Grid item xs={4}>
             <Button
@@ -273,7 +228,12 @@ const AddProjectPopup = (props: AddProjectPopupProps) => {
             >
             저장
             </Button>
-            <Button variant="outlined" color="error">
+            <Button
+              variant="outlined"
+              color="error"
+              onClick={handleDeleteButtonClick}
+              data-index={index}
+            >
               삭제
             </Button>
           </Grid>
