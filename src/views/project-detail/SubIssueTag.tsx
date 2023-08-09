dccf23c4653892da150c84a68e603b586bc8cc48
서빈이 <70849467+leeseobin00@users.noreply.github.com>
2023-08-09 12:18:38 +0900

    feat: Implement additional sub-issues

--- src/views/project-detail/SubIssueTag.tsx
+++ src/views/project-detail/SubIssueTag.tsx
@@ -1,91 +1,150 @@
-// ** MUI Imports
+import React, { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
-import MuiDivider, { DividerProps } from '@mui/material/Divider';
-import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import PlusThick from 'mdi-material-ui/PlusThick';
+import TextField from '@mui/material/TextField';
 
 // ** Custom Components Imports
 import StatusTag, { Status } from './StatusTag';
-
 interface DataType {
-  title: string
-  name: string
+  title: string;
+  name: string;
 }
-
-const issueData = [
-  {
-    title: "버그 수정",
-    name: "이서빈"
-  },
-  {
-    title: "xyz API 개발",
-    name: "이서빈"
-  },
-  {
-    title: "기능 A 수정",
-    name: "이서빈"
-  }
-]
-
-const Divider = styled(MuiDivider)<DividerProps>(({ theme }) => ({
-  margin: theme.spacing(5, 0),
-  borderRight: `1px solid ${theme.palette.divider}`,
-  [theme.breakpoints.down('md')]: {
-    borderRight: 'none',
-    margin: theme.spacing(0, 5),
-    borderBottom: `1px solid ${theme.palette.divider}`
-  }
-}))
 
 const SubIssueTag = () => {
+  const [subIssues, setSubIssues] = useState<DataType[]>([]);
+  const [isAddingSubIssue, setIsAddingSubIssue] = useState(false);
+  const [newSubIssueTitle, setNewSubIssueTitle] = useState('');
+
+  const handleAddSubIssue = () => {
+    setIsAddingSubIssue(true);
+  };
+
+  const handleSaveSubIssue = () => {
+    if (newSubIssueTitle) {
+      const newSubIssue: DataType = {
+        title: newSubIssueTitle,
+        name: '이서빈',
+      };
+      setSubIssues([...subIssues, newSubIssue]);
+      setNewSubIssueTitle('');
+      setIsAddingSubIssue(false);
+    }
+  };
+
   return (
-    <Card sx={{ p: 3, m: 2, backgroundColor: "#e0f2ff" }}>
-      {issueData.map((item: DataType, index: number) => {
-        return (
-          <Card sx={{ position: 'relative', mb: 2, alignItems: 'center' }}>
-            <CardContent
+    <Card sx={{ p: 3, m: 2, backgroundColor: '#e0f2ff' }}>
+      {subIssues.map((item: DataType, index: number) => (
+        <Card
+          key={index}
+          sx={{ position: 'relative', mb: 2, alignItems: 'center' }}
+        >
+          <CardContent
+            sx={{
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <Box
               sx={{
-                flexDirection: 'column',
+                gap: 2,
+                display: 'flex',
+                flexWrap: 'wrap',
+                alignItems: 'center',
                 justifyContent: 'space-between',
-                alignItems: "center"
-              }}>
+              }}
+            >
               <Box
                 sx={{
-                  gap: 2,
+                  mr: 2,
+                  mb: 1,
                   display: 'flex',
-                  flexWrap: 'wrap',
-                  alignItems: 'center',
-                  justifyContent: 'space-between'
+                  flexDirection: 'column',
                 }}
               >
-                <Box sx={{ mr: 2, mb: 1, display: 'flex', flexDirection: 'column' }}>
-                  <Typography >{item.title}</Typography>
-                </Box>
-                <Box sx={{ gap: 2, display: 'flex', flexWrap: 'wrap', justifyContent: 'flex-end', alignItems: 'center' }}>
-                  <Avatar src='/images/avatars/8.png' alt='Alice Cobb' />
-                  <IconButton style={{ borderRadius: 10, padding: 8 }}>
-                    <StatusTag status={Status.InProgress} />
-                  </IconButton>
-                </Box>
+                <Typography>{item.title}</Typography>
               </Box>
-            </CardContent>
-          </Card>)
-      })}
-      <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-        <IconButton style={{ borderRadius: 10 }}>
-          <PlusThick />
-        </IconButton>
-        <Box sx={{ paddingTop: 2, display: 'flex', flexDirection: 'column' }}>
-          <Typography >하위 이슈 추가</Typography>
+              <Box sx={{ gap: 2, display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
+                <Avatar src='/images/avatars/8.png' alt='Alice Cobb' />
+                <IconButton style={{ borderRadius: 10, padding: 8 }}>
+                  <StatusTag status={Status.InProgress} />
+                </IconButton>
+              </Box>
+            </Box>
+          </CardContent>
+        </Card>
+      ))}
+      {isAddingSubIssue ? (
+        <Card sx={{ position: 'relative', mb: 2, alignItems: 'center' }}>
+          <CardContent
+            sx={{
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <Box
+              sx={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+              }}
+            >
+              <Box
+                sx={{
+                  mr: 2,
+                  mb: 1,
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}
+              >
+                <TextField
+                  sx={{ width: '400%', maxWidth: 1100 }}
+                  id="sub_issue_title"
+                  label="하위 이슈 제목"
+                  variant="standard"
+                  fullWidth
+                  value={newSubIssueTitle}
+                  onChange={(e) => setNewSubIssueTitle(e.target.value)}
+                  InputProps={{
+                    inputProps: { 'aria-label': '하위 이슈 제목 입력창' },
+                  }}
+                />
+              </Box>
+              <Box sx={{ display: 'flex' }}>
+                <Button
+                  variant="contained"
+                  sx={{ fontSize: '0.6rem', mr: 4 }}
+                  onClick={handleSaveSubIssue}
+                >
+                  추가
+                </Button>
+              </Box>
+            </Box>
+          </CardContent>
+        </Card>
+      ) : (
+        <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+          <IconButton
+            style={{ borderRadius: 10 }}
+            onClick={handleAddSubIssue}
+          >
+            <PlusThick />
+          </IconButton>
+          <Box sx={{ paddingTop: 2, display: 'flex', flexDirection: 'column' }}>
+            <Typography>하위 이슈 추가</Typography>
+          </Box>
         </Box>
-        </Box>
+      )}
     </Card>
-  )
-}
+  );
+};
 
-export default SubIssueTag
+export default SubIssueTag;
