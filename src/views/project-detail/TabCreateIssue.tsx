8d22516d2284fbfe17655adc89efae119a87b019
서빈이 <70849467+leeseobin00@users.noreply.github.com>
2023-08-09 16:54:31 +0900

    chore: Delete unnecessary code

--- src/views/project-detail/TabCreateIssue.tsx
+++ src/views/project-detail/TabCreateIssue.tsx
@@ -50,13 +50,6 @@ const ButtonStyled = styled(Button)<ButtonProps>(({ theme }) => ({
     width: 80,
 }))
 
-const rows = [
-    createReleaseNoteData(4, 'v 1.2.1', 'BB 기능 추가 및 변경', [{ category: Category.Feature, releaseContent: 'bb 기능 - 기능 설명' }, { category: Category.Changed, releaseContent: '타입 변경' }], '유소연', '2023.06.30'),
-    createReleaseNoteData(3, 'v 1.1.2', 'OO 서비스 중단', [{ category: Category.Deprecated, releaseContent: '~API 중단' }], '이소현', '2023.06.29'),
-    createReleaseNoteData(2, 'v 1.1.1', 'AA 기능 수정 및 버그 해결', [{ category: Category.Fixed, releaseContent: '~ 연동 안되는 버그 수정' }, { category: Category.Changed, releaseContent: '~ 성능 개선' }], '이서빈', '2023.06.26'),
-    createReleaseNoteData(1, 'v 1.0.0', '문서 최초 생성', [{ category: Category.Feature, releaseContent: '신규 기능 추가 / AA 기능 제공' }], '장예경', '2023.06.20')
-]
-
 const TabCreateIssue = () => {
 
     const [activeTab, setActiveTab] = useRecoilState(activeView);
