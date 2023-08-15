1ddcf78b757cc72485c5863f9d5bf1822a09e03c
001021 <00-10-21@naver.com>
2023-08-15 17:16:29 +0900

    fix: Modify document link to open in current window

--- src/views/project-detail/ReleaseNoteDetailModal.tsx
+++ src/views/project-detail/ReleaseNoteDetailModal.tsx
@@ -6,47 +6,57 @@ import { ReleaseNote } from "src/views/project-detail/TabReleaseNote";
 import CategoryTag from "src/views/project-detail/CategoryTag";
 
 interface ReleaseNoteDetailModalProps {
-  releaseNote: ReleaseNote | null;
-  onClose: () => void;
+    releaseNote: ReleaseNote | null;
+    onClose: () => void;
 }
 
 const ReleaseNoteDetailModal: React.FC<ReleaseNoteDetailModalProps> = ({ releaseNote, onClose }) => {
-  const handleClose = () => {
-    onClose();
-  };
+    const handleClose = () => {
+        onClose();
+    };
 
-  return (
-    <Modal
-      open={!!releaseNote}
-      onClose={handleClose}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
-      <Box sx={{backgroundColor: 'white', width: '800px', height: '500px', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', padding: '30px'}}>
-        <Typography id="modal-modal-title" variant="h6" component="h2" sx={{fontWeight: 'bold'}}>
-          {releaseNote?.releaseTitle}
-        </Typography>
-        <Typography id="modal-modal-description" variant="body2" sx={{marginTop: '20px'}}>
-            <strong>Version: </strong>{releaseNote?.version}
-        </Typography>
-        <Typography id="modal-modal-description" variant="body2" sx={{marginTop: '20px'}}>
-            <strong>Creator: </strong>{releaseNote?.creator.userName}
-        </Typography>
-        <Typography id="modal-modal-description" variant="body2" sx={{marginTop: '20px'}}>
-            <strong>Creation Date: </strong>{releaseNote?.creationDate ? new Date(releaseNote?.creationDate).toLocaleDateString(): ""}
-        </Typography>
-        <Typography id="modal-modal-description" variant="body2" sx={{marginTop: '20px'}}>
-          {releaseNote?.releaseContents.map((item, index) => (
-            <Box key={index} sx={{marginBottom: '10px'}}>
-               <CategoryTag category={item.category} />
-              <Typography>{item.releaseSummary}</Typography>
-              {item.documentLink && <Typography component='a' href={item.documentLink} target='_blank' sx={{fontSize: '14px'}}>{item.documentLink}</Typography>}
+    return (
+        <Modal
+            open={!!releaseNote}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+            <Box sx={{ backgroundColor: 'white', width: '800px', height: '500px', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', padding: '30px' }}>
+                <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ fontWeight: 'bold' }}>
+                    {releaseNote?.releaseTitle}
+                </Typography>
+                <Typography id="modal-modal-description" variant="body2" sx={{ marginTop: '20px' }}>
+                    <strong>Version: </strong>{releaseNote?.version}
+                </Typography>
+                <Typography id="modal-modal-description" variant="body2" sx={{ marginTop: '20px' }}>
+                    <strong>Creator: </strong>{releaseNote?.creator.userName}
+                </Typography>
+                <Typography id="modal-modal-description" variant="body2" sx={{ marginTop: '20px' }}>
+                    <strong>Creation Date: </strong>{releaseNote?.creationDate ? new Date(releaseNote?.creationDate).toLocaleDateString() : ""}
+                </Typography>
+                <Typography id="modal-modal-description" variant="body2" sx={{ marginTop: '20px' }}>
+                    {releaseNote?.releaseContents.map((item, index) => (
+                        <Box key={index} sx={{ marginBottom: '10px' }}>
+                            <CategoryTag category={item.category} />
+                            <Typography>{item.releaseSummary}</Typography>
+                            {item.documentLink && (
+                                <Typography
+                                    component="span"
+                                    onClick={() => window.open(item.documentLink, "_blank")}
+                                    sx={{ fontSize: '14px', color: "blue", cursor: 'pointer', "&:hover": { textDecoration: "underline" } }}
+                                >
+                                    {item.documentLink}
+                                </Typography>
+                            )}
+
+
+                        </Box>
+                    ))}
+                </Typography>
             </Box>
-          ))}
-        </Typography>
-      </Box>
-    </Modal>
-  );
+        </Modal>
+    );
 }
 
 export default ReleaseNoteDetailModal;
