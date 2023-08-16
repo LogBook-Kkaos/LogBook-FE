be32c595818eb5f42b8e1068ec0b081ebebc5df4
001021 <00-10-21@naver.com>
2023-08-16 10:15:45 +0900

    feat: Update to react-hook-form

--- src/views/project-detail/TabCreateIssue.tsx
+++ src/views/project-detail/TabCreateIssue.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Select from 'react-select';
+import { useForm, Controller, FieldValues } from 'react-hook-form';
 
 // ** MUI Imports
 import Card from '@mui/material/Card';
@@ -12,14 +12,18 @@ import Button, { ButtonProps } from '@mui/material/Button'
 import Avatar from '@mui/material/Avatar'
 import ArrowLeft from 'mdi-material-ui/ArrowLeft'
 import TextField from '@mui/material/TextField';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from "@mui/material/MenuItem";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
 
 // ** Custom Components Imports
-import CreateIssueCard from './CreateIssueCard';
 import StatusTag, { Status } from "./StatusTag"
 
 // ** Recoil Imports
 import { useRecoilState } from 'recoil';
 import { activeView } from 'src/recoil/issue/atom';
+
 
 const CardWrapper = styled(Box)<BoxProps>({
     width: '33%',
@@ -35,12 +39,21 @@ interface onIssueCreateProps {
     onIssueCreate: any
 }
 
+interface Issue {
+    issueTitle: string,
+    issueDescription: string,
+    assignee: string,
+    status: Status,
+}
+
 const TabCreateIssue = ({ onIssueCreate }: onIssueCreateProps) => {
 
+    const { control, register, handleSubmit, formState: { errors }, reset } = useForm();
+
     const [activeTab, setActiveTab] = useRecoilState(activeView);
-    const [issueTitle, setIssueTitle] = useState('');
     const [selectVisible, setSelectVisible] = useState<boolean>(false);
-    const [selectedValue, setSelectedValue] = useState<string>('');
+    const [assignee, setAssignee] = useState<string>('');
+    const [status, setStatus] = useState<Status>(Status.InProgress);
 
     const options = [
         { value: '이서빈', label: '이서빈' },
@@ -49,8 +62,18 @@ const TabCreateIssue = ({ onIssueCreate }: onIssueCreateProps) => {
         { value: '장예경', label: '장예경' },
     ];
 
+    const statusOptions = [
+        { value: Status.InProgress, label: "진행 중" },
+        { value: Status.NotStarted, label: "아직 시작되지 않음" },
+        { value: Status.Completed, label: "완료됨" },
+    ];
+
+    const handleStatusChange = (selectedOption: any) => {
+        setStatus(selectedOption.value);
+    };
+
+
     const handleCreateIssue = () => {
-        onIssueCreate(issueTitle);
         setActiveTab('issue');
     };
 
@@ -63,88 +86,139 @@ const TabCreateIssue = ({ onIssueCreate }: onIssueCreateProps) => {
     };
 
     const handleSelectChange = (selectedOption: any) => {
-        setSelectedValue(selectedOption.value);
+        setAssignee(selectedOption.value);
         console.log(selectedOption.value)
     };
+
+    const onSubmit = (data: FieldValues) => {
+        console.log({
+            issueTitle: data.issueTitle,
+            issueDescription: data.issueDescription,
+            assignee: assignee,
+            status: status,
+        })
+        setActiveTab('issue');
+    }
 
     return (
         <Grid container justifyContent="flex-start" alignItems="center" >
             <Grid item xs={12} >
-                <Card sx={{ display: 'flex', flexDirection: 'column' }}>
-                    <IconButton sx={{ ml: 8, mt: 5 }} style={{ borderRadius: 10, padding: 8, alignSelf: "self-start" }}
-                        onClick={() => handleTabChange('issue')}>
-                        <ArrowLeft />
-                    </IconButton>
-                    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mt: 5 }}>
-                        <TextField
-                            sx={{ backgroundColor: 'white', ml: 8 }}
-                            id="issue_title"
-                            label="이슈 제목"
-                            variant="outlined"
-                            fullWidth
-                            InputProps={{
-                                inputProps: { 'aria-label': '이슈 제목 입력창' }
-                            }}
-                            value={issueTitle}
-                            onChange={(e) => setIssueTitle(e.target.value)}
-                        />
-                        <Box sx={{ display: 'flex' }}>
-                            <ButtonStyled variant='contained' sx={{ fontSize: '0.8rem' }}
-                                onClick={handleCreateIssue}>
-                                저장
-                                <input
-                                    hidden
-                                    type='file'
-                                    accept='image/png, image/jpeg'
-                                    id='account-settings-upload-image'
-                                />
-                            </ButtonStyled>
-                            <ButtonStyled variant='contained' sx={{ fontSize: '0.8rem', mr: 8 }}>
-                                삭제
-                                <input
-                                    hidden
-                                    type='file'
-                                    accept='image/png, image/jpeg'
-                                    id='account-settings-upload-image'
-                                />
-                            </ButtonStyled>
-                        </Box>
-                    </Box>
-                    <Box sx={{ ml: 10, mt: 4, gap: 2, display: 'flex', flexWrap: 'wrap', justifyContent: 'flex-start', alignItems: 'center' }}>
-                        <Avatar src='/images/avatars/8.png' alt={selectedValue || '없음'}
-                            onClick={(event) => {
-                                event.stopPropagation();
-                                handleAvatarClick();
-                            }} />
-                        <IconButton style={{ borderRadius: 10, padding: 8 }}>
-                            <StatusTag status={Status.InProgress} />
+                <form onSubmit={handleSubmit((data) => onSubmit(data))}>
+                    <Card sx={{ display: 'flex', flexDirection: 'column' }}>
+                        <IconButton sx={{ ml: 8, mt: 5 }} style={{ borderRadius: 10, padding: 8, alignSelf: "self-start" }}
+                            onClick={() => handleTabChange('issue')}>
+                            <ArrowLeft />
                         </IconButton>
-                        {selectVisible && (
+                        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mt: 5 }}>
+                            <TextField
+                                sx={{ backgroundColor: 'white', ml: 8 }}
+                                {...register('issueTitle', { required: true })}
+                                id="issue_title"
+                                label="이슈 제목"
+                                variant="outlined"
+                                fullWidth
+                                InputProps={{
+                                    inputProps: { 'aria-label': '이슈 제목 입력창' }
+                                }}
+                            />
+                            {errors.issueTitle && <span>이슈 제목을 입력해주세요</span>}
+                            <Box sx={{ display: 'flex' }}>
+                                <ButtonStyled variant='contained' sx={{ fontSize: '0.8rem' }}
+                                    onClick={handleCreateIssue}>
+                                    저장
+                                    <input
+                                        hidden
+                                        type='file'
+                                        accept='image/png, image/jpeg'
+                                        id='account-settings-upload-image'
+                                    />
+                                </ButtonStyled>
+                                <ButtonStyled variant='contained' sx={{ fontSize: '0.8rem', mr: 8 }}>
+                                    삭제
+                                    <input
+                                        hidden
+                                        type='file'
+                                        accept='image/png, image/jpeg'
+                                        id='account-settings-upload-image'
+                                    />
+                                </ButtonStyled>
+                            </Box>
+                        </Box>
+                        <Box sx={{ ml: 10, mt: 4, gap: 2, display: 'flex', flexWrap: 'wrap', justifyContent: 'flex-start', alignItems: 'center' }}>
+                            <Avatar src='/images/avatars/8.png' alt={assignee || '없음'}
+                                onClick={handleAvatarClick} />
                             <Box
                                 sx={{
                                     position: 'relative',
                                     width: "150px",
                                     mt: 1,
                                     py: 1,
-                                    backgroundColor: '#fff',
-                                    boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',
                                     zIndex: 5000
                                 }}
                             >
-                                <Select options={options}
-                                    menuIsOpen
-                                    onMenuClose={() => setSelectVisible(false)}
-                                    onChange={handleSelectChange}
-                                />
+                                <FormControl sx={{ minWidth: 120, mt: 1 }}>
+                                    <InputLabel id="assignee-select-label">담당자</InputLabel>
+                                    <Select
+                                        labelId="assignee-select-label"
+                                        open={selectVisible}
+                                        onClose={() => setSelectVisible(false)}
+                                        onOpen={() => setSelectVisible(true)}
+                                        value={assignee}
+                                        onChange={(event: SelectChangeEvent) => handleSelectChange(event.target.value as string)}
+                                    >
+                                        {options.map((option) => (
+                                            <MenuItem key={option.value} value={option.value}>
+                                                {option.label}
+                                            </MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+
+
                             </Box>
-                        )}
-                    </Box>
-                    <CardContent sx={{ display: 'flex', justifyContent: 'center', gap: '0rem', alignItems: 'stretch' }}>
-                        <CardWrapper sx={{ width: '100%' }}>
-                            <CreateIssueCard />
-                        </CardWrapper>
-                    </CardContent>
-                </Card>
+                            <FormControl sx={{ minWidth: 120, mt: 2 }}>
+                                <InputLabel id="status-select-label">이슈 상태</InputLabel>
+                                <Select
+                                    labelId="status-select-label"
+                                    value={status}
+                                    onChange={(event: SelectChangeEvent) => handleStatusChange(event.target.value as Status)}
+                                >
+                                    {statusOptions.map((option) => (
+                                        <MenuItem key={option.value} value={option.value}>
+                                            {option.label}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+
+                            <Box sx={{ mt: 2 }}>
+                                <StatusTag status={status} />
+                            </Box>
+                        </Box>
+
+                        <CardContent sx={{ display: 'flex', justifyContent: 'center', gap: '0rem', alignItems: 'stretch' }}>
+                            <CardWrapper sx={{ width: '100%' }}>
+                                <Card sx={{ p: 1, m: 2, backgroundColor: "#e0f2ff" }}>
+                                    <Box sx={{ minWidth: 38, display: 'flex', justifyContent: 'left', p: '1.25rem' }}>
+                                        <TextField
+                                            sx={{ backgroundColor: 'white' }}
+                                            {...register('issueDescription', { required: true })}
+                                            id="issue_content"
+                                            label="이슈 내용 설명"
+                                            multiline
+                                            rows={8}
+                                            variant="outlined"
+                                            fullWidth
+                                            InputProps={{
+                                                inputProps: { 'aria-label': '이슈 내용 입력창' }
+                                            }}
+                                        />
+                                    </Box>
+                                </Card>
+                            </CardWrapper>
+                        </CardContent>
+                    </Card>
+                </form>
             </Grid>
         </Grid>
     )
